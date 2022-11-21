4dc54d59f07d5933a34ac9087140e892a053e22b
jaeyson <hello@jaeyson.dev>
2022-11-22 07:03:42 +0800

    initial commit

---
+++ lib/ex_typesense.ex
@@ -0,0 +1,18 @@
+defmodule ExTypesense do
+  @moduledoc """
+  Documentation for `ExTypesense`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTypesense.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_typesense_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTypesenseTest do
+  use ExUnit.Case
+  doctest ExTypesense
+
+  test "greets the world" do
+    assert ExTypesense.hello() == :world
+  end
+end
