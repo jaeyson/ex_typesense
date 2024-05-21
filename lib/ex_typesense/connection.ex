d95b986f4e1089ee352ae0227ee47adc1be24422
jaeyson <hello@jaeyson.dev>
2024-05-21 14:37:45 +0800

    add option to add map instead of connection struct

--- lib/ex_typesense/connection.ex
+++ lib/ex_typesense/connection.ex
@@ -1,4 +1,9 @@
 defmodule ExTypesense.Connection do
+  @moduledoc since: "0.4.0"
+  @moduledoc """
+  Fetches credentials either from application env or map.
+  """
+
   @derive {Inspect, except: [:api_key]}
   defstruct host: ExTypesense.HttpClient.get_host(),
             api_key: ExTypesense.HttpClient.api_key(),
@@ -7,7 +12,11 @@ defmodule ExTypesense.Connection do
 
   @type t() :: %__MODULE__{}
 
-  @spec new(connection :: struct()) :: ExTypesense.Connection.t()
+  @doc """
+  Fetches credentials either from application env or map.
+  """
+  @doc since: "0.4.0"
+  @spec new(connection :: struct() | map()) :: ExTypesense.Connection.t()
   def new(connection \\ %__MODULE__{}) when is_struct(connection) do
     %__MODULE__{
       host: Map.get(connection, :host),
