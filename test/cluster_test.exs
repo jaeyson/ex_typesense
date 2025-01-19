870e8b87041f3d7cf9862fe0cb82199a90202cf2
jaeyson <hello@jaeyson.dev>
2025-01-19 14:04:47 +0800

    remove connection_test.exs

--- test/cluster_test.exs
+++ test/cluster_test.exs
@@ -1,18 +1,109 @@
 defmodule ClusterTest do
   use ExUnit.Case, async: true
 
+  alias ExTypesense.TestSchema.Credential
+  alias OpenApiTypesense.ApiResponse
   alias OpenApiTypesense.APIStatsResponse
   alias OpenApiTypesense.Connection
   alias OpenApiTypesense.HealthStatus
   alias OpenApiTypesense.SuccessStatus
 
   @rate_limit :timer.seconds(5)
+  @forbidden %ApiResponse{
+    message: "Forbidden - a valid `x-typesense-api-key` header must be sent."
+  }
 
   setup_all do
     conn = Connection.new()
     map_conn = %{api_key: "xyz", host: "localhost", port: 8108, scheme: "http"}
 
     %{conn: conn, map_conn: map_conn}
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "error: health check with empty credentials" do
+    conn = %{api_key: nil, host: nil, port: nil, scheme: nil}
+
+    assert_raise FunctionClauseError, fn ->
+      ExTypesense.health(conn)
+    end
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "new/1 with invalid data type raises ArgumentError" do
+    invalid_inputs = [
+      nil,
+      "string",
+      123,
+      [],
+      [host: "localhost"]
+    ]
+
+    for input <- invalid_inputs do
+      error =
+        assert_raise ArgumentError, fn ->
+          Connection.new(input)
+        end
+
+      assert error.message === "Expected a map for connection options"
+    end
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "error: health check, with incorrect port number" do
+    conn = %{api_key: "xyz", host: "localhost", port: 8100, scheme: "http"}
+
+    assert {:error, "connection refused"} = ExTypesense.health(conn)
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "error: health check, with incorrect host" do
+    conn = %{api_key: "xyz", host: "my_test_host", port: 8108, scheme: "http"}
+
+    assert {:error, "non-existing domain"} = ExTypesense.health(conn)
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "error: wrong api key was configured" do
+    conn = %{
+      host: "localhost",
+      api_key: "another_key",
+      port: 8108,
+      scheme: "http"
+    }
+
+    assert {:error, @forbidden} == ExTypesense.list_collections(conn)
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "error: overriding config with a wrong API key" do
+    conn = %{
+      host: "localhost",
+      api_key: "another_key",
+      port: 8108,
+      scheme: "http"
+    }
+
+    assert {:error, @forbidden} = ExTypesense.list_collections(conn)
+  end
+
+  @tag ["27.1": true, "27.0": true, "26.0": true]
+  test "success: Using a struct converted to map and update its keys" do
+    conn = %Credential{
+      node: "localhost",
+      secret_key: "xyz",
+      port: 8108,
+      scheme: "http"
+    }
+
+    conn =
+      conn
+      |> Map.from_struct()
+      |> Map.drop([:node, :secret_key])
+      |> Map.put(:host, conn.node)
+      |> Map.put(:api_key, conn.secret_key)
+
+    assert {:ok, %HealthStatus{ok: true}} = ExTypesense.health(conn)
   end
 
   @tag ["27.1": true, "27.0": true, "26.0": true]
