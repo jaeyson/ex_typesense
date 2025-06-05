eebc1b3cac769f205bc705d80317ddee31cd608c
Andrew <andrewianstewart@gmail.com>
2025-06-05 12:32:57 -0400

    Add dialyxir for static analysis

--- mix.exs
+++ mix.exs
@@ -39,6 +39,7 @@ defmodule ExTypesense.MixProject do
     [
       {:ex_doc, "~> 0.34", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ecto, "~> 3.12"},
       {:excoveralls, "~> 0.18", only: :test, runtime: false},
       {:mix_audit, "~> 2.1", only: :test, runtime: false},
