0de7f376025df318af5c28ddaabfe6a20322a81c
jaeyson <hello@jaeyson.dev>
2026-06-09 17:18:02 +0800

    Use forked version of Poison

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ExTypesense.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/jaeyson/ex_typesense"
-  @version "2.3.1"
+  @version "2.3.2"
 
   def project do
     [
@@ -46,14 +46,7 @@ defmodule ExTypesense.MixProject do
       {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:open_api_typesense, "~> 1.3"},
-      {:ecto, "~> 3.13"},
-
-      # URL: https://github.com/advisories/GHSA-rhv4-8758-jx7v
-      # Title: Decimal: Unbounded exponent in `Decimal.new` enables unauthenticated DoS
-      # Severity: moderate
-      # Vulnerable versions: >= 0.1.0, < 3.0.0
-      # First patched versions: 3.0.0
-      {:decimal, "~> 3.0", override: System.get_env("HEX_PUBLISH") != "true"}
+      {:ecto, "~> 3.13"}
     ]
   end
 
